dc637b42d67966841fa71578f06b42b7c4140638
Raj-yadav-7822 <ry682734@gmail.com>
2025-12-22 21:53:36 +0530

    post

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -54,7 +54,7 @@ const Login = () => {
     <div className='flex items-center w-screen h-screen justify-center'>
       <form onSubmit={loginHandler} className='shadow-lg flex flex-col gap-5 p-8'>
         <div className='my-4'>
-          <h1 className='text-center font-bold text-xl'>LOGO</h1>
+          <h1 className='text-center font-bold text-xl'>InstaVibe</h1>
           <p className='text-sm text-center'>Login to see photos & videos from your friends</p>
         </div>
 
